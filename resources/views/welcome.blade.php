66700598f3e74292c5ede69481c041b4c638d7dd
Ryan Babij <rbabij@gmail.com>
2020-05-07 06:02:06 +1000

    Test Google Chart API

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,6 +120,8 @@
 
 
                     <li class="navbar"><a href="login">Login</a></li>
+                    <li class="navbar"><a href="login">Statistics (bigquery)</a></li>
+					
                 </ul>
             </nav>
         </div>
@@ -127,6 +129,33 @@
     </div>
 	
 	<div id="map" style="height:800px;"></div>
+	
+	
+	
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+var data = google.visualization.arrayToDataTable([
+['Task', 'Hours per Day'],
+['Work', 11],
+['Eat', 2],
+['Commute', 2],
+['Watch TV', 2],
+['Sleep', 7]
+]);
+var options = {
+title: 'My Daily Activities'
+};
+var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+chart.draw(data, options);
+}
+</script>
+</head>
+<body>
+<div id="piechart" style="width: 900px; height: 500px;"></div>
 
     <!-- Scripts -->
     <script src="assets/js/jquery.min.js"></script>
