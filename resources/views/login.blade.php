4480aea961db3ffc52b5e8d4a190cdf537bd21aa
Ryan Babij <rbabij@gmail.com>
2020-05-07 20:39:22 +1000

    Revert "Remove Laravel"
    This reverts commit 864e44a63c69fa69908336bd9cb5a58fda038d90.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    
+    <h1>Admin login</h1>
+
+
+	
+	<p>Login:</p>
+	
+	<form action="login_check" method="POST">
+	Username: <input type="text" name="username" id="username" maxlength="20" required>
+	<br/><br/>
+	<!--Password: <input type="password" name="password" id="password">-->
+	Password: <input type="text" name="password" id="password" maxlength="20" required>
+	<br/><br/>
+	<button type="submit">Login</button>
+	</form>
+	
+	<a href="login_check">login check</a>
+	<a href="register">register</a>
+</body>
+</html>
