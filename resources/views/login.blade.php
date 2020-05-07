ab95dad85e4dc91689133b3d81917031cbf4685e
Ryan Babij <rbabij@gmail.com>
2020-05-07 18:53:05 +1000

    Add login forms

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,8 +7,22 @@
 </head>
 <body>
     
-    This is the login page
+    <h1>Admin login</h1>
 
 
+	
+	<p>Login:</p>
+	
+	<form action="login_check" method="POST">
+	Username: <input type="text" name="username" id="username" maxlength="20" required>
+	<br/><br/>
+	<!--Password: <input type="password" name="password" id="password">-->
+	Password: <input type="text" name="password" id="password" maxlength="20" required>
+	<br/><br/>
+	<button type="submit">Login</button>
+	</form>
+	
+	<a href="login_check">login check</a>
+	<a href="register">register</a>
 </body>
 </html>
