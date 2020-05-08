d6e88d3cf4298ec6074a63e3d849ab835c61214e
Ryan Babij <rbabij@gmail.com>
2020-05-08 14:57:57 +1000

    Update main.php

--- main.php
+++ main.php
@@ -99,57 +99,50 @@ $_SESSION["passSucc"] = True;
 	
     <div id="map"></div>
     <script>
-      // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
-      var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-			
-			// navigator.geolocation.getCurrentPosition(
-				// function(position) {
-					 // //alert("Lat: " + position.coords.latitude + "\nLon: " + position.coords.longitude);
-				// },
-				// function(error){
-					 // //alert(error.message);
-				// }, {
-					 // enableHighAccuracy: true
-							// ,timeout : 5000
-				// }
-			// );
-			
-          navigator.geolocation.getCurrentPosition(
-		  
-		  function(position)
-		  { 
-			var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
-
-			infoWindow.setPosition(pos);
-			infoWindow.setContent('Location found.');
-			infoWindow.open(map);
-			map.setCenter(pos);
-			},
-			function()
+		// Note: This example requires that you consent to location sharing when
+		// prompted by your browser. If you see the error "The Geolocation service
+		// failed.", it means you probably did not give permission for the browser to
+		// locate you.
+		var map, infoWindow;
+		function initMap()
+		{
+			map = new google.maps.Map(document.getElementById('map'),
 			{
-				handleLocationError(true, infoWindow, map.getCenter());
-			},
+				center: {lat: -37, lng: 145},
+				zoom: 10
+			});
+			infoWindow = new google.maps.InfoWindow;
+
+			// Try HTML5 geolocation.
+			if (navigator.geolocation)
 			{
-				enableHighAccuracy: true,timeout : 5000
-			}		  
-		  );
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+				navigator.geolocation.getCurrentPosition
+				(
+					function(position)
+					{
+						alert("Lat: " + position.coords.latitude + "\nLon: " + position.coords.longitude);
+						var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
+
+						infoWindow.setPosition(pos);
+						infoWindow.setContent('Location found.');
+						infoWindow.open(map);
+						map.setCenter(pos);
+					},
+					function()
+					{
+						handleLocationError(true, infoWindow, map.getCenter());
+					},
+					{
+						enableHighAccuracy: true,timeout : 5000
+					}		  
+				);
+			}
+			else
+			{
+				// Browser doesn't support Geolocation
+				handleLocationError(false, infoWindow, map.getCenter());
+			}
+		}
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
