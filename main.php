6dddc78a03c9a85de3c675ff1af23155ce43d1b5
Ryan Babij <rbabij@gmail.com>
2020-05-08 15:37:39 +1000

    Try using maps ssl

--- main.php
+++ main.php
@@ -163,54 +163,54 @@ $_SESSION["passSucc"] = True;
 	  
 	  
 	  
-var apiGeolocationSuccess = function(position) {
-    alert("API geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
-};
+// var apiGeolocationSuccess = function(position) {
+    // alert("API geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
+// };
 
-var tryAPIGeolocation = function() {
-    jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDCa1LUe1vOczX1hO_iGYgyo8p_jYuGOPU", function(success) {
-        apiGeolocationSuccess({coords: {latitude: success.location.lat, longitude: success.location.lng}});
-  })
-  .fail(function(err) {
-    alert("API Geolocation error! \n\n"+err);
-  });
-};
+// var tryAPIGeolocation = function() {
+    // jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDCa1LUe1vOczX1hO_iGYgyo8p_jYuGOPU", function(success) {
+        // apiGeolocationSuccess({coords: {latitude: success.location.lat, longitude: success.location.lng}});
+  // })
+  // .fail(function(err) {
+    // alert("API Geolocation error! \n\n"+err);
+  // });
+// };
 
-var browserGeolocationSuccess = function(position) {
-    alert("Browser geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
-};
+// var browserGeolocationSuccess = function(position) {
+    // alert("Browser geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
+// };
 
-var browserGeolocationFail = function(error) {
-  switch (error.code) {
-    case error.TIMEOUT:
-      alert("Browser geolocation error !\n\nTimeout.");
-      break;
-    case error.PERMISSION_DENIED:
-      if(error.message.indexOf("Only secure origins are allowed") == 0) {
-        tryAPIGeolocation();
-      }
-      break;
-    case error.POSITION_UNAVAILABLE:
-      alert("Browser geolocation error !\n\nPosition unavailable.");
-      break;
-  }
-};
+// var browserGeolocationFail = function(error) {
+  // switch (error.code) {
+    // case error.TIMEOUT:
+      // alert("Browser geolocation error !\n\nTimeout.");
+      // break;
+    // case error.PERMISSION_DENIED:
+      // if(error.message.indexOf("Only secure origins are allowed") == 0) {
+        // tryAPIGeolocation();
+      // }
+      // break;
+    // case error.POSITION_UNAVAILABLE:
+      // alert("Browser geolocation error !\n\nPosition unavailable.");
+      // break;
+  // }
+// };
 
-var tryGeolocation = function() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        browserGeolocationSuccess,
-      browserGeolocationFail,
-      {maximumAge: 50000, timeout: 20000, enableHighAccuracy: true});
-  }
-};
+// var tryGeolocation = function() {
+  // if (navigator.geolocation) {
+    // navigator.geolocation.getCurrentPosition(
+        // browserGeolocationSuccess,
+      // browserGeolocationFail,
+      // {maximumAge: 50000, timeout: 20000, enableHighAccuracy: true});
+  // }
+// };
 
-tryGeolocation();
+// tryGeolocation();
 
 
     </script>
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9PBXwqzgcNzIT6AfY45hMrsmYlhB2_ro&callback=initMap">
+    src="https://maps.google.com/maps/api/js?v=3.5&sensor=true&callback=initMap">
     </script>
 	
 	
