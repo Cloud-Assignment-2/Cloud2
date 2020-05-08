e335f1fd7b2793527c2777aa1524e261ebea45e1
Ryan Babij <rbabij@gmail.com>
2020-05-08 14:48:36 +1000

    Update main.php

--- main.php
+++ main.php
@@ -114,17 +114,17 @@ $_SESSION["passSucc"] = True;
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
 			
-			// navigator.geolocation.getCurrentPosition(
-				// function(position) {
-					 // alert("Lat: " + position.coords.latitude + "\nLon: " + position.coords.longitude);
-				// },
-				// function(error){
-					 // alert(error.message);
-				// }, {
-					 // enableHighAccuracy: true
-							// ,timeout : 5000
-				// }
-			// );
+			navigator.geolocation.getCurrentPosition(
+				function(position) {
+					 //alert("Lat: " + position.coords.latitude + "\nLon: " + position.coords.longitude);
+				},
+				function(error){
+					 //alert(error.message);
+				}, {
+					 enableHighAccuracy: true
+							,timeout : 5000
+				}
+			);
 			
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
