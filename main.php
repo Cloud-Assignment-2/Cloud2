da53d9c2d796c7db4407217c0d3275c9f4cc3b48
Ryan Babij <rbabij@gmail.com>
2020-05-08 16:09:52 +1000

    Update main.php

--- main.php
+++ main.php
@@ -161,9 +161,8 @@ $_SESSION["passSucc"] = True;
 		}
     </script>
 	
-    <script async defer
-    src="https://maps-api-ssl.google.com/maps/api/js?key=AIzaSyC9PBXwqzgcNzIT6AfY45hMrsmYlhB2_ro&callback=initMap">
-    </script>
+	<script async defer type="text/javascript"
+	src="//maps-api-ssl.google.com/maps/api/js?sensor=false&key=AIzaSyC9PBXwqzgcNzIT6AfY45hMrsmYlhB2_ro&callback=initMap"></script>
 	
 	
 	
