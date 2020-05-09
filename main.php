8f3f4141c661933c2f4deabea7779dbf05cde5b1
Ryan Babij <rbabij@gmail.com>
2020-05-09 16:59:35 +1000

    remove all for debug

--- main.php
+++ main.php
@@ -5,125 +5,18 @@ $_SESSION["passSucc"] = True;
 ?>
 
 <!DOCTYPE HTML>
-<!--
-	Forty by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
+
 <html>
 
 <head>
     <title>Fitness Tracker</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <noscript>
-        <link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-
-    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
-
-
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
 </head>
 
-<body class="is-preload">
-
-    <!-- Wrapper -->
-    <div id="wrapper">
-
-
-        <!-- Banner -->
-        <section id="banner" class="major">
-            <div class="inner">
-                <header class="major">
-                    <center>
-                        <h1 id="title">Fitness Tracker</h1>
-                    </center>
-                </header>
-            </div>
-        </section>
-		
-<h1>Welcome, <?php echo $_SESSION["login_id"]; ?></h1>
-<p>
-<a href="/name.php">Change name</a>
-<a href="/password.php">Change password</a>
-</p>
-        <div class="center">
-            <nav>
-                <ul>
-                    <style>
-                    .center {
-                        text-align: center;
-                    }
-
-
-                    .navbar {
-                        text-align: center;
-                        display: inline;
-                        margin: 0;
-                        padding: 10px;
-                        font-family: "Source Sans Pro", Helvetica, sans-serif;
-
-                        font-size: 1.7em;
-                    }
-                    </style>
-
-                    <li class="navbar"><a href="login">Login</a></li>
-                    <li class="navbar"><a href="login">Statistics (bigquery)</a></li>
-					
-                </ul>
-            </nav>
-        </div>
-
-    </div>
-	
-
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-var data = google.visualization.arrayToDataTable([
-['Task', 'Hours per Day'],
-['Work', 11],
-['Eat', 2],
-['Commute', 2],
-['Watch TV', 2],
-['Sleep', 7]
-]);
-var options = {
-title: 'Test chart (can use this for user profile)'
-};
-var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-chart.draw(data, options);
-}
-</script>
-</head>
 <body>
-<div id="piechart" style="width: 900px; height: 500px;"></div>
-
-    <!-- Scripts -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery.scrolly.min.js"></script>
-    <script src="assets/js/jquery.scrollex.min.js"></script>
-    <script src="assets/js/browser.min.js"></script>
-    <script src="assets/js/breakpoints.min.js"></script>
-    <script src="assets/js/util.js"></script>
-    <script src="assets/js/main.js"></script>
-
+<h1 id="title">Fitness Tracker</h1>
+<p>test</p>	
 </body>
 
 </html>
