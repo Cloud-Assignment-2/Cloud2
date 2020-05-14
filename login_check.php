595e866cfd539af65ce37a514b5a09b28558e79b
Ryan Babij <rbabij@gmail.com>
2020-05-14 22:51:34 +1000

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
 ob_start();
-
-	if (strcmp($username,"admin") === 0 && strcmp($password,"admin") === 0)
-	{
-		// password is correct, redirect to cp
-		header('Location: main.php');
-		exit();
-	}
-
-header('Location: index.php');
-exit();
 ?>
 
 <script>
