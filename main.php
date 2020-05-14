e161483c10bcb80a009c501319dd54d06769e3e6
Ryan Babij <rbabij@gmail.com>
2020-05-14 20:49:33 +1000

    Try node.js

--- main.php
+++ main.php
@@ -147,6 +147,45 @@ $datastore = new DatastoreClient([
 		}
 		echo $count;
 	?>
+	
+	<script>
+// Imports the Google Cloud client library
+const Datastore = require('@google-cloud/datastore');
+
+// Your Google Cloud Platform project ID
+const projectId = 'cloudfit';
+
+// Creates a client
+const datastore = new Datastore({
+  projectId: projectId,
+});
+
+// The kind for the new entity
+const kind = 'Task';
+// The name/ID for the new entity
+const name = 'sampletask1';
+// The Cloud Datastore key for the new entity
+const taskKey = datastore.key([kind, name]);
+
+// Prepares the new entity
+const task = {
+  key: taskKey,
+  data: {
+    description: 'Buy milk',
+  },
+};
+
+// Saves the entity
+datastore
+  .save(task)
+  .then(() => {
+    console.log(`Saved ${task.key.name}: ${task.data.description}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+	</script>
+	
 	</p>
 
 	<br/>
