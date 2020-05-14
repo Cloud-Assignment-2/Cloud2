fcdb7eba299e3ba291d30a018637e02e1685bb9d
Ryan Babij <rbabij@gmail.com>
2020-05-14 22:47:17 +1000

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -12,3 +12,33 @@ ob_start();
 header('Location: index.php');
 exit();
 ?>
+
+<script>
+db.collection("user").get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                if (doc.data().available) {
+                    var coordinates = {
+                        lat: doc.data().location.latitude,
+                        lng: doc.data().location.longitude
+                    };
+					
+					
+// Create a reference to the cities collection
+//var citiesRef = db.collection("user");
+
+// Create a query against the collection.
+//var query = citiesRef.where("user", "==", '<?php echo $username; ?>');
+
+db.collection("user").where("user", "==", '"admin")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log("SUCC");
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+					
+</script>
