a5a2627abcc2d66cfb3c62e8a17b7b73fd9e1ad0
Ryan Babij <rbabij@gmail.com>
2020-05-14 19:18:23 +1000

    Add datastore test

--- main.php
+++ main.php
@@ -2,6 +2,44 @@
 session_start();
 $_SESSION["nameSucc"] = True;
 $_SESSION["passSucc"] = True;
+
+//Receive username from client side
+$entered_username = $_POST['username'];
+//Receive password from client side
+$entered_password = $_POST['password'];
+
+require __DIR__ . '/vendor/autoload.php';
+use Google\Cloud\Datastore\DatastoreClient;
+
+$projectId = 'cloudfit';
+$datastore = new DatastoreClient([
+'projectId' => $projectId
+]);
+
+echo "DATABASE CONNECTION SUCC";
+
+// $key = $datastore->key('user', $entered_username);
+// $entity = $datastore->lookup($key);
+// if (!is_null($entity))
+// {
+	// $database_pwd = $entity['password'];
+	
+	// //echo $database_pwd;
+	// //echo $entered_password;
+	// if (strcmp($database_pwd,$entered_password) === 0)
+	// {
+		// $_SESSION["loginSucc"] = False;
+			// // save name and key for later
+		// $_SESSION["login_id"] = $entered_username;
+		// $_SESSION["login_name"] = $entity['name'];
+		// // password is correct, redirect to main page
+		// header('Location: main.php');
+		// exit();
+	// }
+// }
+// $_SESSION["loginSucc"] = True;
+// header('Location: login.php');
+
 ?>
 
 <!DOCTYPE HTML>
