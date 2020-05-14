df9fa094d9e76a4f3d654673046b7955e3a9b409
Ryan Babij <rbabij@gmail.com>
2020-05-14 22:08:14 +1000

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -1,52 +1,9 @@
 <?php
 session_start();
 
-//Receive username from client side
-$entered_username = $_POST['username'];
-//Receive password from client side
-$entered_password = $_POST['password'];
-
-if ( $entered_password == "" && $entered_username == "" )
-{
+	$_SESSION["login_id"] = 'admin';
+	$_SESSION["login_name"] = 'admin';
+	// redirect to main page
 	header('Location: main.php');
 	exit();
-}
-
-require __DIR__ . '/vendor/autoload.php';
-use Google\Cloud\Datastore\DatastoreClient;
-
-$projectId = 'cloudfit';
-$datastore = new DatastoreClient([
-'projectId' => $projectId
-]);
-
-$key = $datastore->key('User', $entered_username);
-$entity = $datastore->lookup($key);
-if (!is_null($entity))
-{
-	$database_pwd = $entity['password'];
-
-	if (strcmp($database_pwd,$entered_password) === 0)
-	{
-		$_SESSION["loginSucc"] = False;
-			// save name and key for later
-		$_SESSION["login_id"] = $entered_username;
-		$_SESSION["login_name"] = $entity['name'];
-		// password is correct, redirect to main page
-		header('Location: main.php');
-		//echo "login success";
-		exit();
-	}
-}
-else
-{
-	//echo "null";
-}
-$_SESSION["loginSucc"] = True;
-header('Location: index.php');
-
-//echo "login fail";
-
 ?>
-
-<p>Hello</p>
