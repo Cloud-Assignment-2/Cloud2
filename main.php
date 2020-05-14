0e14ec1959567358f7a61e4aa8d9512fd804f71f
Ryan Babij <rbabij@gmail.com>
2020-05-14 20:53:07 +1000

    Update main.php

--- main.php
+++ main.php
@@ -24,6 +24,20 @@ $datastore = new DatastoreClient([
 
 ?>
 
+	<script>
+// Imports the Google Cloud client library
+const Datastore = require('@google-cloud/datastore');
+
+// Your Google Cloud Platform project ID
+const projectId = 'cloudfit';
+
+// Creates a client
+const datastore = new Datastore({
+  projectId: projectId,
+});
+
+	</script>
+
 <!DOCTYPE HTML>
 
 <html>
@@ -147,44 +161,6 @@ $datastore = new DatastoreClient([
 		}
 		echo $count;
 	?>
-	
-	<script>
-// Imports the Google Cloud client library
-const Datastore = require('@google-cloud/datastore');
-
-// Your Google Cloud Platform project ID
-const projectId = 'cloudfit';
-
-// Creates a client
-const datastore = new Datastore({
-  projectId: projectId,
-});
-
-// The kind for the new entity
-const kind = 'Task';
-// The name/ID for the new entity
-const name = 'sampletask1';
-// The Cloud Datastore key for the new entity
-const taskKey = datastore.key([kind, name]);
-
-// Prepares the new entity
-const task = {
-  key: taskKey,
-  data: {
-    description: 'Buy milk',
-  },
-};
-
-// Saves the entity
-datastore
-  .save(task)
-  .then(() => {
-    console.log(`Saved ${task.key.name}: ${task.data.description}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
-	</script>
 	
 	</p>
 
