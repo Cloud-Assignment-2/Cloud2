ae496c7c213ffb47c4371163830bf2307e095182
Ryan Babij <rbabij@gmail.com>
2020-05-14 22:27:02 +1000

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -5,10 +5,10 @@ ob_start();
 	if (strcmp($username,"admin") === 0 && strcmp($password,"admin") === 0)
 	{
 		// password is correct, redirect to cp
-		header('Location: https://car-for-all-273711.appspot.com/cp');
+		header('Location: main.php');
 		exit();
 	}
 
-header('Location: https://car-for-all-273711.appspot.com/admin');
+header('Location: index.php');
 exit();
 ?>
