81c8ddcf950c8ea19f55b0bad3a4f7ef83bed6e4
Ryan Babij <rbabij@gmail.com>
2020-05-14 22:24:00 +1000

    Add javascript auth

--- login_check.php
+++ login_check.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
+ob_start();
 
-	$_SESSION["login_id"] = 'admin';
-	$_SESSION["login_name"] = 'admin';
-	// redirect to main page
-	header('Location: main.php');
-	exit();
+	if (strcmp($username,"admin") === 0 && strcmp($password,"admin") === 0)
+	{
+		// password is correct, redirect to cp
+		header('Location: https://car-for-all-273711.appspot.com/cp');
+		exit();
+	}
+
+header('Location: https://car-for-all-273711.appspot.com/admin');
+exit();
 ?>
