7a65965a2a0fa8106649eeb4323340fc22247779
Ryan Babij <rbabij@gmail.com>
2020-05-16 18:04:19 +1000

    Update main.php

--- main.php
+++ main.php
@@ -271,21 +271,21 @@ ob_start();
 	var interval = setInterval(updateLoop, 15000);
 	
 	// Get distance between two geopoints
-	public static float distFrom (float lat1, float lng1, float lat2, float lng2 ) 
-	{
-		double earthRadius = 3958.75;
-		double dLat = Math.toRadians(lat2-lat1);
-		double dLng = Math.toRadians(lng2-lng1);
-		double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-		Math.sin(dLng/2) * Math.sin(dLng/2);
-		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		double dist = earthRadius * c;
+	// public static float distFrom (float lat1, float lng1, float lat2, float lng2 ) 
+	// {
+		// double earthRadius = 3958.75;
+		// double dLat = Math.toRadians(lat2-lat1);
+		// double dLng = Math.toRadians(lng2-lng1);
+		// double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		// Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+		// Math.sin(dLng/2) * Math.sin(dLng/2);
+		// double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		// double dist = earthRadius * c;
 
-		int meterConversion = 1609;
+		// int meterConversion = 1609;
 
-		return new Float(dist * meterConversion).floatValue();
-	}
+		// return new Float(dist * meterConversion).floatValue();
+	// }
 	
     </script>
     <script async defer
