57f3d434f76e8d3620aae6b779a159815cbd42e2
Ryan Babij <rbabij@gmail.com>
2020-05-16 18:26:23 +1000

    Update main.php

--- main.php
+++ main.php
@@ -276,11 +276,11 @@ ob_start();
 	function distFrom (lat1, lng1, lat2, lng2 ) 
 	{
 		var earthRadius = 3958.75;
-		//var dLat = Math.toRadians(lat2-lat1);
-		//var dLng = Math.toRadians(lng2-lng1);
-		// double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		// Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-		// Math.sin(dLng/2) * Math.sin(dLng/2);
+		var dLat = Math.toRadians(lat2-lat1);
+		var dLng = Math.toRadians(lng2-lng1);
+		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+		Math.sin(dLng/2) * Math.sin(dLng/2);
 		// double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 		// double dist = earthRadius * c;
 
