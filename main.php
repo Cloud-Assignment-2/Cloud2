de98dda0f37b6b2cdedd4f62cb24b51155e6ba07
Ryan Babij <rbabij@gmail.com>
2020-05-22 21:24:04 +1000

    Update main.php

--- main.php
+++ main.php
@@ -62,8 +62,7 @@ ob_start();
 	var removeDistantID = "none"; // distant marker flagged to remove
 	var removeCloseID = "none"; // close marker flagged to remove
 	
-	// Elevation service
-	var elevator = new google.maps.ElevationService;
+
 
 	var userPos =
 	{
@@ -151,6 +150,8 @@ ob_start();
 	
 	function updateElevation()
 	{
+		// Elevation service
+		var elevator = new google.maps.ElevationService;
 		//var elevationData = https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536,-104.9847034&key=YOUR_API_KEY
 		
 		// Initiate the location request
