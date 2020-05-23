a109009212b1ca52990f7a80c992d83f656a77b4
Ryan Babij <rbabij@gmail.com>
2020-05-23 21:42:13 +1000

    Update main.php

--- main.php
+++ main.php
@@ -519,12 +519,12 @@ ob_start();
 		console.log("marker added to db");
 	}
 	
-	// var origin1 = new google.maps.LatLng(55.930385, -3.118425);
-	// var origin2 = 'Greenwich, England';
-	// var destinationA = 'Stockholm, Sweden';
-	// var destinationB = new google.maps.LatLng(50.087692, 14.421150);
+	var origin1 = new google.maps.LatLng(55.930385, -3.118425);
+	var origin2 = 'Greenwich, England';
+	var destinationA = 'Stockholm, Sweden';
+	var destinationB = new google.maps.LatLng(50.087692, 14.421150);
 
-	// var service = new google.maps.DistanceMatrixService();
+	var service = new google.maps.DistanceMatrixService();
 	// service.getDistanceMatrix(
 	// {
 	// origins: [origin1, origin2],
